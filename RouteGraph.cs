422bb58cb3c5164fd8240b29f721f7fb8053839d
NarkoPraxis <narkopraxisgaming@gmail.com>
2018-06-24 18:37:42 -0600

    Initial Unit Tests to validate eventual code design
    I expect most of this to change but it helps me think through the problem and formulate what I'll need as well as provides easy ways to test each part as i finish it.

---
+++ RouteGraph.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedirectsExercise
+{
+    public class RouteGraph : RouteAnalyzer
+    {
+        public List<List<int>> Graph { get; set; } = new List<List<int>>();
+        public Dictionary<string, int> Identifiers { get; set; } = new Dictionary<string, int>();
+
+        private int uniqueIdentifier = 0;
+        private string[] delimiters = { "->", " "};
+
+        public RouteGraph() {
+
+        }
+    
+        //I have to inject these values for the class to be unit testable... 
+        public RouteGraph(List<List<int>> graph, Dictionary<string, int> identifiers) {
+            Graph = graph;
+            Identifiers = identifiers;
+            uniqueIdentifier = identifiers.Count;
+        }
+
+        public IEnumerable<string> Process(IEnumerable<string> routes) {
+            foreach (string path in routes) {
+                MapPath(path);
+            }
+            return TraverseGraph();
+        }
+
+        public void MapPath(string path) { 
+            List<string> routeNames = ParseRoutes(path);
+            //if the end of this route path already exists in the graph
+            if (Identifiers.Keys.Contains(routeNames[routeNames.Count - 1])) {
+                int id = Identifiers[routeNames[routeNames.Count - 1]];
+                int pathIndex = GetPathIndex(id);
+
+                //add the new path to the graph
+                Graph.Add(new List<int>());
+                for (int i = 0; i < routeNames.Count - 1; i++) {
+                    if (CreatesCircularReference(routeNames[i], pathIndex)) {
+                        throw new ArgumentException("Circular Path Detected, infinite loop eminent");
+                    }
+                    else {
+                        Identifiers[routeNames[i]] = uniqueIdentifier;
+                        Graph[EndGraphIndex].Add(uniqueIdentifier++);
+                    }
+                }
+                //add the old path on the end of the graph
+                foreach(int routeID in Graph[pathIndex]) {
+                    Graph[EndGraphIndex].Add(routeID);
+                }
+                //remove the old path
+                Graph.RemoveAt(pathIndex);
+            }
+            //if the beginning of this route path already exists in the graph
+            else if (Identifiers.Keys.Contains(routeNames[0])) {
+                int id = Identifiers[routeNames[0]];
+                int pathIndex = GetPathIndex(id);
+
+                for (int i = 1; i < routeNames.Count; i++) {
+                    if (CreatesCircularReference(routeNames[i], pathIndex)) { 
+                        throw new ArgumentException("Circular Path Detected, infinite loop eminent");
+                    }
+                    else {
+                        Identifiers[routeNames[i]] = uniqueIdentifier;
+                        Graph[pathIndex].Add(uniqueIdentifier++);
+                    }
+                }
+            }
+            //this route path is unique
+            else {
+                Graph.Add(new List<int>());
+                foreach (string route in routeNames) {
+                    Identifiers[route] = uniqueIdentifier;
+                    Graph[EndGraphIndex].Add(uniqueIdentifier++);
+                }
+            }
+        }
+
+        public int GetPathIndex(int id) {
+            for (int i = 0; i < Graph.Count; i++) {
+                List<int> path = Graph[i];
+                if (id == path[path.Count - 1] || id < path[path.Count - 1]) {
+                    return i;
+                }
+            }
+            return -1; // couldn't find path, return invalid path index
+        }
+
+        public List<string> TraverseGraph() {
+            List<string> output = new List<string>();
+            string redirects = "";
+            foreach(List<int> path in Graph) {
+                redirects = string.Join(" <- ", path);
+                output.Add(redirects);
+            }
+            return output;
+        }
+        
+        public bool CreatesCircularReference(string routeName, int pathIndex) {
+            if (Identifiers.Keys.Contains(routeName)) {
+                int id = Identifiers[routeName];
+                return Graph[pathIndex].Contains(Identifiers[routeName]);
+            }
+            return false;
+        }
+        
+        public List<string> ParseRoutes(string path) {
+            return path.Split(delimiters, System.StringSplitOptions.RemoveEmptyEntries).ToList(); 
+        }
+
+        public int EndGraphIndex {
+            get {
+                return Graph.Count - 1;
+            }
+        }
+    }
+}
---
+++ UnitTest.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedirectsExercise;
+
+namespace RedirectUnitTests
+{
+    /// <summary>
+    /// Tests the functions of the RouteGraph Tests
+    /// </summary>
+    [TestClass]
+    public class RouteGraphTests
+    {
+        #region Lists for testing
+        List<string> sampleInput = new List<string>() {
+            "/home",
+            "/our-ceo.html -> /about-us.html",
+            "/about-us.html -> /about",
+            "/product-1.html -> /seo"
+        };
+
+        List<List<string>> finishedSampleInputGraph = new List<List<string>>() {
+            new List<string>() {"/home" },
+            new List<string>() {"/our-ceo.html", "/about-us.html", "/about" },
+            new List<string>() {"/product-1.html", "seo" }
+        };
+
+        List<string> largerInput = new List<string>() {
+            "/home -> /kitchen",
+            "/kitchen ->/sink -> /dishes",
+            "/our-ceo.html -> /about-us.html",
+            "/about-us.html -> /about -> /faq",
+            "/faq -> /info",
+            "/product-1.html -> /seo -> /google",
+            "/google -> /images -> /filter",
+        };
+
+        List<List<string>> finishedLargerInputGraph = new List<List<string>>() {
+            new List<string>() {"/home", "/kitchen", "/sink", "/dishes" },
+            new List<string>() {"/our-ceo.html", "/about-us.html", "/about", "/faq", "/info"},
+            new List<string>() {"/product-1.html", "/seo", "/google", "/images", "filter"}
+        };
+
+        List<string> shuffledInput = new List<string>() {
+            "/google -> /images -> /filter",
+            "/our-ceo.html -> /about-us.html",
+            "/kitchen -> /sink -> /dishes",
+            "/product-1.html -> /seo -> /google",
+            "/about-us.html -> /about -> /faq",
+            "/faq -> /info",
+            "/home -> /kitchen",
+        };
+
+        List<string> invalidInput = new List<string>() {
+            "/google -> /images -> /filter",
+            "/our-ceo.html -> /about-us.html",
+            "/info -> /faq",
+            "/kitchen -> /sink -> /dishes",
+            "/product-1.html -> /seo -> /google",
+            "/about-us.html -> /about -> /faq",
+            "/faq -> /info",
+            "/home -> /kitchen",
+        };
+
+
+        List<List<string>> midProcessGraph = new List<List<string>>() {
+            new List<string>() { "/home" },
+            new List<string>() {"/our-ceo.html", "/about-us.html" }
+        };
+
+        List<List<string>> midProcessFinishedGraph = new List<List<string>>() {
+            new List<string>() { "/home" },
+            new List<string>() {"/our-ceo.html", "/about-us.html", "/about" }
+        };
+
+
+
+        #endregion
+
+        /// <summary>
+        /// The current graph being used for testing
+        /// </summary>
+        RouteGraph _routeGraph = new RouteGraph();
+
+        //sets up a test before running it.
+        public void SetupForTest(List<List<string>> initialGraph = null) {
+            _routeGraph.Graph = initialGraph;
+        }
+
+        /// <summary>
+        /// Tests from beginning to end that correct input can be processed correctly,
+        /// </summary>
+        [TestMethod]
+        public void ProcessCorrectInputTest() {
+            try {
+                _routeGraph.Process(shuffledInput);
+                CompareGraphs(finishedLargerInputGraph);
+            }
+            catch {
+                Assert.Fail();
+            }
+        }
+
+        /// <summary>
+        /// Tests the if the RouteGraph.Insert() function can find the right posi
+        /// Does this happen as i expect?
+        /// </summary>
+        [TestMethod]
+        public void InsertTest() {
+            SetupForTest(midProcessGraph);
+            _routeGraph.MapRoute("/about-us.html -> /about");
+            CompareGraphs(midProcessFinishedGraph);
+            
+        }
+
+        /// <summary>
+        /// This Tests putting the graph back into the expected output for a redirect path
+        /// Can I find the route path I want to find?
+        /// </summary>
+        [TestMethod]
+        public void GetPathTest() {
+            SetupForTest(finishedLargerInputGraph);
+            Assert.Equals(_routeGraph.GetPath(0), "/home -> /kitchen -> /sink -> /dishes");
+            Assert.Equals(_routeGraph.GetPath(1), "/our-ceo.html -> /about-us.html -> /about -> /faq -> /info");
+            Assert.Equals(_routeGraph.GetPath(2), "/product-1.html -> /seo -> /google -> /images -> /filter");
+        }
+
+        /// <summary>
+        /// This tests gettings a single redirect from the graph
+        /// </summary>
+        [TestMethod]
+        public void GetRedirectTest() {
+            SetupForTest(finishedLargerInputGraph);
+            Assert.Equals(_routeGraph.GetRedirect(0, 0), "/home");
+            Assert.Equals(_routeGraph.GetRedirect(2, 5), "/filter");
+            Assert.Equals(_routeGraph.GetRedirect(1, 0), "/our-ceo.html");
+        }
+
+        /// <summary>
+        /// This tests wether or not I can determine if a given input
+        /// would create a cirular dependancy. I need to write specific tests, but
+        /// I don't understand enough about how to do this yet.
+        /// 
+        /// validate this.
+        /// </summary>
+        [TestMethod]
+        public void IsCircularReferenceTest() {
+            SetupForTest();
+            try {
+                _routeGraph.Process(invalidInput);
+            }
+            catch (Exception exc) {
+                Assert.IsTrue(exc.GetType() == typeof(ArgumentException));
+            }
+            Assert.Fail();
+        }
+
+        private void CompareGraphs(List<List<string>> testGraph)
+        {
+            for (int i = 0; i < _routeGraph.Graph.Count; i++) {
+                List<string> path = _routeGraph.Graph[i];
+                for (int k = 0; k < path.Count; k++) {
+                    Assert.AreEqual(path[k], testGraph[i][k]);
+                }
+            }
+        }
+
+    }
+}
